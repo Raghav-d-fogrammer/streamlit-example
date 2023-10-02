6943588eff9383d88ba5353764e4c241641ab5cc
Raghav-d-fogrammer <117929469+Raghav-d-fogrammer@users.noreply.github.com>
2023-10-02 19:15:06 +0000

    fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-col1, col2 = st.columns(2)
-
-with col1:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col2:
-   st.header("Chat")
-   prompt = st.chat_input("Talk to the Doc!!!")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
